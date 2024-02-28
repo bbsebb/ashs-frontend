88c593a9f50330eb95f36def80734d8b0e789c3b
Sébastien Burckhardt <sebastien.burckhardt@gmail.com>
2024-02-28 13:06:25 +0100

    add route to home in logo

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,11 +1,14 @@
-import {Component, inject, OnInit, Signal, signal} from '@angular/core';
+import {Component, effect, inject, Injector, OnInit, Signal, signal} from '@angular/core';
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
 import {MatAnchor, MatIconAnchor, MatIconButton} from "@angular/material/button";
 import {SidenavOpeningService} from "../../services/sidenav-opening.service";
 import {NgOptimizedImage} from "@angular/common";
 import {RouterLink} from "@angular/router";
-import {ScrollDispatcher} from "@angular/cdk/overlay";
+import {ScrollDispatcher, ViewportRuler} from "@angular/cdk/overlay";
+import {of, startWith, switchMap, tap} from "rxjs";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'app-header',
@@ -24,16 +27,25 @@ import {ScrollDispatcher} from "@angular/cdk/overlay";
 })
 export class HeaderComponent implements OnInit {
   sidenavOpeningService = inject(SidenavOpeningService);
+  private injector = inject(Injector);
 
 
   showToolbar: Signal<boolean> = signal(true);
-
-  constructor(private scrollDispatcher: ScrollDispatcher) {
+  switchToolbar: Signal<boolean | undefined> = signal(true);
+  constructor(private scrollDispatcher: ScrollDispatcher,private viewportRuler:ViewportRuler) {
 
   }
 
   ngOnInit(): void {
-    this.scrollDispatcher.scrolled().subscribe(x => console.log(window.scrollY));
+    this.switchToolbar = toSignal<boolean>(this.scrollDispatcher.scrolled()
+      .pipe(
+        startWith(void 0),
+        map(()=> this.viewportRuler.getViewportScrollPosition().top),
+        map((top) => top < 100)
+      ), {injector: this.injector});
+    effect(() => {
+      console.log(`The current count is: ${this.switchToolbar()}`);
+    },{injector: this.injector});
   }
 
 
