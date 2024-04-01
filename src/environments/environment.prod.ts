1b0b90e31c2b1715dc955d43d42db0334081fc7f
Sébastien Burckhardt <sebastien.burckhardt@gmail.com>
2024-04-01 02:46:11 +0200

    config CI/CD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  firebaseConfig:  {
-    apiKey: "${{ secrets.FIREBASE_API_KEY }}",
-    authDomain: "${{ secrets.FIREBASE_AUTH_DOMAIN }}",
-    projectId: "${{ secrets.FIREBASE_PROJECT_ID }}",
-    storageBucket: "${{ secrets.FIREBASE_STORAGE_BUCKET }}",
-    messagingSenderId: "${{ secrets.FIREBASE_MESSAGING_SENDER_ID }}",
-    appId: "${{ secrets.FIREBASE_APP_ID }}",
-    measurementId: "${{ secrets.FIREBASE_MEASUREMENT_ID }}"
-  }
+  firebaseConfig:  "FIREBASE_CONFIG_PLACEHOLDER"
 };
