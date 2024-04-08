184d6d88f8b4146c29cad5114c9b4c997a4c9965
Sébastien Burckhardt <sebastien.burckhardt@gmail.com>
2024-04-09 00:27:08 +0200

    set deploy github action

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production:true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'https://api.hoenheimsports.fr',
   firebaseConfig:  "FIREBASE_CONFIG_PLACEHOLDER"
 };
